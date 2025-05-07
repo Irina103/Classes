7cf4d6ec728045a3097f8794ce4df4a361688b2e
onlyirina <79596699+Irina103@users.noreply.github.com>
2025-05-07 15:02:18 +0400

    Update __innit__.py

--- Class_folder/__innit__.py
+++ Class_folder/__innit__.py
@@ -1,1 +1,15 @@
-from .class_ import 
+from .class1.py import (
+    Glass,
+    Table,
+    Backpack,
+    Battery,
+    SongLibrary,
+    Rectangle,
+    Circle,
+    Tickets,
+    Book,
+    PaperBook,
+    AudioBook,
+    Library,
+    func,  # if func is a utility function you want to expose
+)
