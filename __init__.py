a0b342a40bd12e6a6f1cf86333f33c70334cbc93
onlyirina <79596699+Irina103@users.noreply.github.com>
2025-05-23 16:25:17 +0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .module1 import count_word_occurrences, most_frequent_word
+from .module2 import get_previous_word_indices, create_dict_from_range
+from .module3 import calculate_product_sum, find_elements_with_frequency
+
+__all__ = [
+           'count_word_occurrences', 'most_frequent_word',
+           'get_previous_word_indices', 'create_dict_from_range',
+           'calculate_product_sum', 'find_elements_with_frequency'
+          ]
